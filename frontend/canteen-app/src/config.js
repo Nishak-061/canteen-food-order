99bea3b8e178ffc052d7df6ea218dcb1ac1c6ce3
Nisha Kumari <nk.nishakumari71@gmail.com>
2024-07-01 23:46:14 +0530

    last commit

--- frontend/canteen-app/src/config.js
+++ frontend/canteen-app/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE_URL: "https://canteen-food-order-3.onrender.com",
+    API_BASE_URL: "https://canteen-food-order-final.onrender.com",
   };
   
   export default config;
