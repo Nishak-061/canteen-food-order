9d88b7fe45d6f3a20fa895a9b6923cb0f5ad5174
Nisha Kumari <nk.nishakumari71@gmail.com>
2024-07-01 19:21:55 +0530

    food-app

--- frontend/canteen-app/src/hooks/useCategory.js
+++ frontend/canteen-app/src/hooks/useCategory.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import config from "../config";
 
 export default function useCategory() {
   const [categories, setCategories] = useState([]);
